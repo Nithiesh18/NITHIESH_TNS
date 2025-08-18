9c05fe54526ec0ba75e8ab8c59cd3a43a9aff4d4
Nithiesh <124070486+Nithiesh18@users.noreply.github.com>
2025-08-18 06:03:48 +0530

    Commit

---
+++ TNS_Project5/src/interfacedemo/GreetingDemo.java
@@ -0,0 +1,7 @@
+package data.tnsif.interfacedemo;
+
+@FunctionalInterface
+public interface GreetingDemo {
+	void greet(); //used for declaration
+	
+}
---
+++ TNS_Project5/src/multithreading/package-info.java
@@ -0,0 +1,1 @@
+package data.tnsif.multithreading;
